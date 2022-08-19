08199be4cbf310c2a942740f286a3158137d887f
cristiam <cristiam9211@gmail.com>
2022-08-18 20:50:17 -0500

    first init

---
+++ src/main/java/com/example/ejemplo1/Ejemplo1Application.java
@@ -0,0 +1,15 @@
+package com.example.ejemplo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ejemplo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ejemplo1Application.class, args);
+
+		//comentario nuevo comentario
+	}
+
+}
